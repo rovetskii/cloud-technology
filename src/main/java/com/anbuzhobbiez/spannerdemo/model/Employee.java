425e0c08d10feb58bcf153d9e936750cc0509daa
Ivan_Rovetskyi <rovetskii.ivan@gmail.com>
2024-02-09 15:18:09 +0200

    simple spring microservice

--- src/main/java/com/anbuzhobbiez/spannerdemo/model/Employee.java
+++ src/main/java/com/anbuzhobbiez/spannerdemo/model/Employee.java
@@ -1,15 +1,13 @@
 package com.anbuzhobbiez.spannerdemo.model;
 
-import com.google.cloud.spring.data.spanner.core.mapping.PrimaryKey;
-import com.google.cloud.spring.data.spanner.core.mapping.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Table(name = "employee")
+//@Table(name = "employee")
 @Data
 @AllArgsConstructor
 public class Employee {
-    @PrimaryKey
+   // @PrimaryKey
     private String id;
     private String name;
 }
